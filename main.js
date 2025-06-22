80dc44243d32980a8af8d6f30d714bc98e933de2
Muhammad Nurrahman Juliansyah <166163781+MuhammadNur02@users.noreply.github.com>
2025-06-22 11:41:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*==================== toggle icon navbar ====================*/
+/==================== toggle icon navbar ====================/;
 
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
@@ -8,51 +8,56 @@ menuIcon.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-/*==================== scroll section active link ====================*/
+/==================== scroll section active link ====================/;
 
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
+  let top = window.scrollY;
+
   sections.forEach((sec) => {
-    let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach.apply((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classListadd("active");
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
       });
+      document
+        .querySelector('header nav a[href*="' + id + '"]')
+        .classList.add("active");
     }
   });
 
-  /*==================== sticky navbar ====================*/
-
+  /==================== sticky navbar ====================/;
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 100);
 
-  /*==================== remove toggle icon and navbaar ====================*/
-
+  /==================== remove toggle icon and navbar when link clicked ====================/;
   menuIcon.classList.remove("fa-xmark");
   navbar.classList.remove("active");
 };
 
-/*==================== scroll reveal ====================*/
+/==================== scroll reveal ====================/;
 
-ScrollReveal({ distance: "80px", duration: 2000, delay: 200 });
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+  reset: true, // opsional: jika ingin animasi saat kembali scroll
+});
 
-ScrollReveal().reveal("home-content, heading", { origin: "top" });
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
 ScrollReveal().reveal(
-  "home-img, service-container, .portfolio-box, .contact form",
-  { origin: "button" }
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
 );
-ScrollReveal().reveal("home-contact h1, .about-img", { origin: "left" });
-ScrollReveal().reveal("home-contact p, .about-content", { origin: "right" });
-/*==================== typed js ====================*/
+ScrollReveal().reveal(".about-img", { origin: "left" });
+ScrollReveal().reveal(".about-content", { origin: "right" });
+
+/==================== typed js ====================/;
 const typed = new Typed(".multiple-text", {
   strings: ["Frontend Developer", "Web Designer", "UI/UX Designer"],
   typeSpeed: 70,
