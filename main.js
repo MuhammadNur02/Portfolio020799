0bd64bfaca2833c470873c1c0eda40977bf14389
Muhammad Nurrahman Juliansyah <166163781+MuhammadNur02@users.noreply.github.com>
2025-06-24 12:47:41 +0700

    Update main.js
    Signed-off-by: Muhammad Nurrahman Juliansyah <166163781+MuhammadNur02@users.noreply.github.com>

--- main.js
+++ main.js
@@ -65,3 +65,14 @@ const typed = new Typed(".multiple-text", {
   backDelay: 1000,
   loop: true,
 });
+ScrollReveal({
+  reset: false,
+  distance: '60px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
